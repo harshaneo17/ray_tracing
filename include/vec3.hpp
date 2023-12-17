44d147b44fcf07bb3d0ea93fed226baa258a0be0
harshaneo17 <harsha.god86@gmail.com>
2023-12-17 18:08:46 +0000

    changed double to float

--- include/vec3.hpp
+++ include/vec3.hpp
@@ -4,23 +4,35 @@
 #include <cmath>
 #include <iostream>
 
+/* header guards are definded to ensure multiple includes are not being called during compilation
+This is a preprocessor technique of preventing a header file from being included multiple times, 
+which can be problematic for various reasons. 
+During compilation of your project, each .cpp file (usually) is compiled. 
+In simple terms, this means the compiler will take your .cpp file, open any files #included by it, 
+concatenate them all into one massive text file, and then perform syntax analysis 
+and finally it will convert it to some intermediate code, optimize/perform other tasks, 
+and finally generate the assembly output for the target architecture. Because of this, 
+if a file is #included multiple times under one .cpp file, the compiler will append its file contents twice, 
+so if there are definitions within that file, you will get a compiler error telling you that you redefined a variable.
+read more here https://shorturl.at/diHV2
+*/
 
 class vec3 {
   public :
-    double e[3]; //e is an arrya of 3 elements of double
+    float e[3]; //e is an arrya of 3 elements of float
 
     vec3() : e{0,0,0} {} //constructor 1 to create vec3 with all components to zero
-    vec3(double e0, double e1, double e2) : e{e0, e1, e3} {} // another constructor is provided that takes three doubles as parameters
+    vec3(float e0, float e1, float e2) : e{e0, e1, e3} {} // another constructor is provided that takes three floats as parameters
 
-    double x() const {
+    float x() const {
          return e[0]; 
     }
 
-    double y() const {
+    float y() const {
          return e[1]; 
     }
 
-    double z() const {
+    float z() const {
          return e[2];
     }
 
@@ -28,11 +40,11 @@ class vec3 {
         return vec3(-e[0], -e[1], -e[2]); 
     }
 
-    double operator[](int i) const { 
+    float operator[](int i) const { 
         return e[i];
     }
 
-    double& operator[](int) {
+    float& operator[](int) {
         return e[i];
     }
 
@@ -43,22 +55,22 @@ class vec3 {
         return *this;
     }
 
-    vec3& operator*=(double t){
+    vec3& operator*=(float t){
         e[0] *= t;
         e[1] *= t;
         e[2] *= t;
         return *this;
     }
 
-    vec3& operator/=(double t) {
+    vec3& operator/=(float t) {
         return *this *= 1/t;
     }
 
-    double length() const{
+    float length() const{
         return std::sqrt(length_squared());
     }
 
-    double length_squared() const {
+    float length_squared() const {
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     } 
 };
@@ -92,15 +104,15 @@ inline vec3 operator*(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
-inline vec3 operator*(double t, const vec3 &v) {
+inline vec3 operator*(float t, const vec3 &v) {
     return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
-inline vec3 operator/(vec3 v, double t){
+inline vec3 operator/(vec3 v, float t){
     return (1/t) * v;
 }
 
-inline double dot(const vec3 &u, const vec3 &v){
+inline float dot(const vec3 &u, const vec3 &v){
     return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2]; 
 }
 
@@ -113,3 +125,5 @@ inline vec3 cross(const vec3 &u, const vec3 &v){
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
+
+#endif
