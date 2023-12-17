b40d23e39fb91ed86241bd73756b04d6ba2f7e7b
harshaneo17 <harsha.god86@gmail.com>
2023-12-17 17:17:43 +0000

    changes to formatting

--- include/vec3.hpp
+++ include/vec3.hpp
@@ -12,13 +12,29 @@ class vec3 {
     vec3() : e{0,0,0} {}
     vec3(double e0, double e1, double e2) : e{e0, e1, e3} {}
 
-    double x() const { return e[0]; }
-    double y() const { return e[1]; }
-    double z() const { return e[2]; }
+    double x() const {
+         return e[0]; 
+    }
 
-    vec3 operator-() const {return vec3(-e[0], -e[1], -e[2]); }
-    double operator[](int i) const { return e[i];}
-    double& operator[](int) {return e[i];}
+    double y() const {
+         return e[1]; 
+    }
+    
+    double z() const {
+         return e[2];
+    }
+
+    vec3 operator-() const {
+        return vec3(-e[0], -e[1], -e[2]); 
+    }
+
+    double operator[](int i) const { 
+        return e[i];
+    }
+
+    double& operator[](int) {
+        return e[i];
+    }
 
     vec3& operator+=(const vec3 &v) {
         e[0] += v.e[0];
@@ -32,5 +48,18 @@ class vec3 {
         e[1] *= t;
         e[2] *= t;
         return *this;
+    }
+
+    vec3& operator/=(double t) {
+        return *this *= 1/t;
+    }
+
+    double length() const{
+        return std::sqrt(length_squared());
+    }
+
+    double length_squared() const {
+        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     } 
-}
+};
+
