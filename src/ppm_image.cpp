2b469f7a3cef3705cd2e7528d1ec6fa8f9d3bc67
harshaneo17 <harsha.god86@gmail.com>
2023-12-24 13:18:50 +0000

    changes to gradient

--- src/ppm_image.cpp
+++ src/ppm_image.cpp
@@ -5,7 +5,9 @@
 #include <iostream>
 
 Color ray_color(const Ray& r){
-    return Color(0,0,0);
+    Vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y()+1.0);
+    return (1.0-a)*Color(1.0,1.0,1.0)+a*Color(0.5,0.7,1.0);
 }
 
 int main() {
@@ -21,27 +23,26 @@ int main() {
     image height is 1 if image height is less than one else image height stays the same
     */
    
-   //camera
-   float focal_length = 1.0;
-   float viewport_height = 2.0;
-   auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
-   auto camera_center = Point3(0,0,0);
+    //camera
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+    auto camera_center = Point3(0,0,0);
 
-   //calculate the vectors that navigate on the viewport
-   auto viewport_u = Vec3(viewport_width, 0, 0);
-   auto viewport_v = Vec3(0,-viewport_height,0); //why negative?
+    //calculate the vectors that navigate on the viewport
+    auto viewport_u = Vec3(viewport_width, 0, 0);
+    auto viewport_v = Vec3(0,-viewport_height,0); //why negative?
 
-   //calculate the difference in pixel to pixel (Delta v)
-   auto pixel_delta_u = viewport_u / image_width;
-   auto pixel_delta_v = viewport_v / image_height;
+    //calculate the difference in pixel to pixel (Delta v)
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
 
-   //calculate the location of the upper left pixel
-   auto viewport_upper_left = camera_center - Vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
-   auto pixel_origin = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    //calculate the location of the upper left pixel
+    auto viewport_upper_left = camera_center - Vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel_origin = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-   for (int j = 0; j < image_height; ++j){
+    for (int j = 0; j < image_height; ++j){
        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
        for (int i = 0; i < image_width; ++i){
             auto pixel_center = pixel_origin + (i * pixel_delta_u) + (j * pixel_delta_v);
