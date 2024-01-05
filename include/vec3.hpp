f2c4cf8e1dba99d0d4b57f675789e2ea2ca4f613
harshaneo17 <harsha.god86@gmail.com>
2024-01-05 23:53:26 +0000

    added new function

--- include/vec3.hpp
+++ include/vec3.hpp
@@ -149,6 +149,14 @@ inline Vec3 unit_vector(Vec3 v) {
     return v / v.length();
 }
 
+inline Vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = Vec3(random_double(-1,1), random_double(-1,1),0);
+        if (p.length_squared() < 1)
+            return p;
+    }
+}
+
 inline Vec3 random_in_unit_sphere(){
     while(true){
         auto p = Vec3::random_vec(-1,1);
